8d7628f368e4a3c663bb3327f122ae0c2c7066d2
Hebert Campos <hebertcampos@hotmail.com.br>
2019-07-19 19:11:48 -0300

    Listando informações do aluno

--- gremio_imprimi/lib/screens/home_screen.dart
+++ gremio_imprimi/lib/screens/home_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:gremio_imprimi/form/add_aluno_form.dart';
-import 'package:gremio_imprimi/model/crud.dart';
+import 'package:gremio_imprimi/screens/sit_aluno_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -43,11 +43,8 @@ class _HomeScreenState extends State<HomeScreen> {
                   style: TextStyle(fontSize: 22.0, fontWeight: FontWeight.bold),
                 ),
                 onPressed: () {
-                  //getData();
-                  print('oioioioioioioi');
-
                   Navigator.of(context).push(MaterialPageRoute(
-                      builder: (context) => Home(ctrlMatricula.text)));
+                      builder: (context) => SitAluno(ctrlMatricula.text)));
                 })
           ],
         ),
@@ -56,54 +53,4 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 }
 
-class Home extends StatelessWidget {
-  final String matricula;
-  Home(this.matricula);
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: FutureBuilder<List>(
-        future: getData(),
-        builder: (context, snapshot) {
-          if (snapshot.hasError) print(snapshot.error);
-
-          // int j = 0;
-          // for (int i = 0; i < snapshot.data.length; i++) {
-          //   if (matricula == snapshot.data[i]['MATRICULA_ALUNO']) {
-          //     print(i);
-          //     j = i;
-          //   }
-          // }
-
-          return snapshot.hasData
-              ? ListView.builder(
-                  itemCount:
-                      snapshot.data == null ? 0 : 1, //snapshot.data.length,
-                  itemBuilder: (context, i) {
-                    int j = 0;
-                    for (int i = 0; i < snapshot.data.length; i++) {
-                      if (matricula == snapshot.data[i]['MATRICULA_ALUNO']) {
-                        print(i);
-                        j = i;
-                      }
-                    }
-                    if (matricula == snapshot.data[j]['MATRICULA_ALUNO']) {
-                      return Text(
-                        'O aluno ${snapshot.data[j]['NOME_ALUNO']} tem ${snapshot.data[j]['COPIAS_ALUNO']} copias esta semana',
-                        style: TextStyle(
-                            fontSize: 22.0, fontWeight: FontWeight.bold),
-                      );
-                    } else {
-                      return Text('nao encontrado');
-                    }
-                  },
-                )
-              : Center(
-                  child: CircularProgressIndicator(),
-                );
-        },
-      ),
-    );
-  }
-}
--- gremio_imprimi/lib/screens/sit_aluno_screen.dart
+++ gremio_imprimi/lib/screens/sit_aluno_screen.dart
@@ -1,20 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:gremio_imprimi/model/crud.dart';
 
-class SitAlunoScreen extends StatefulWidget {
-  final List list;
-  final int index;
 
-  SitAlunoScreen({this.list, this.index});
-
-  @override
-  _SitAlunoScreenState createState() => _SitAlunoScreenState();
-}
-
-class _SitAlunoScreenState extends State<SitAlunoScreen> {
-  List list;
-  int index;
-
-  // SitAlunoScreen({this.list, this.index});
+class SitAluno extends StatelessWidget {
+  final String matricula;
+  SitAluno(this.matricula);
 
   @override
   Widget build(BuildContext context) {
@@ -25,10 +15,63 @@ class _SitAlunoScreenState extends State<SitAlunoScreen> {
       ),
       body: Container(
         padding: EdgeInsets.all(8.0),
-        child: ListView(
-          children: <Widget>[
-            Text('${widget.list[widget.index]['NOME_ALUNO']}')
-          ],
+        child: FutureBuilder<List>(
+          future: getData(),
+          builder: (context, snapshot) {
+            if (snapshot.hasError) print(snapshot.error);
+            return snapshot.hasData
+                ? ListView.builder(
+                    itemCount: snapshot.data == null ? 0 : 1,
+                    itemBuilder: (context, i) {
+                      int j = 0;
+                      for (int i = 0; i < snapshot.data.length; i++) {
+                        if (matricula == snapshot.data[i]['MATRICULA_ALUNO']) {
+                          print(i);
+                          j = i;
+                        }
+                      }
+                      if (matricula == snapshot.data[j]['MATRICULA_ALUNO']) {
+                        return Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              'O aluno:',
+                              style: TextStyle(
+                                  fontSize: 22.0, fontWeight: FontWeight.bold),
+                            ),
+                            Text(
+                              '    ${snapshot.data[j]['NOME_ALUNO']}',
+                              style: TextStyle(fontSize: 18.0),
+                            ),
+                            Text(
+                              'Turma do aluno:',
+                              style: TextStyle(
+                                  fontSize: 22.0, fontWeight: FontWeight.bold),
+                            ),
+                            Text(
+                              '    ${snapshot.data[j]['TURMA_ALUNO']}',
+                              style: TextStyle(fontSize: 18.0),
+                            ),
+                            Text(
+                              'Nº de copias esta semana:',
+                              style: TextStyle(
+                                  fontSize: 22.0, fontWeight: FontWeight.bold),
+                            ),
+                            Text(
+                              '    ${snapshot.data[j]['COPIAS_ALUNO']}',
+                              style: TextStyle(fontSize: 18.0),
+                            ),
+                          ],
+                        );
+                      } else {
+                        return Text('nao encontrado');
+                      }
+                    },
+                  )
+                : Center(
+                    child: CircularProgressIndicator(),
+                  );
+          },
         ),
       ),
     );
